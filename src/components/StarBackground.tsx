21d8e57e028ef1c44c75e14916aa8823f6b1b88e
Hoàng Nam <bapnhn1810@gmail.com>
2025-09-12 15:34:45 +0700

    Completed Star background

---
+++ src/components/StarBackground.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+//id, size, x, y, opacity, animationDuration
+type Star = {
+  id: number;
+  size: number;
+  x: number;
+  y: number;
+  opacity: number;
+  animationDuration: number;
+};
+
+export const StarBackground = () => {
+  const [stars, setStars] = useState<Star[]>([]); // 👈 quan trọng nè
+
+  useEffect(() => {
+    generateStars();
+  }, []);
+
+  const generateStars = () => {
+    const numberOfStars = Math.floor(
+      (window.innerWidth * window.innerHeight) / 10000
+    );
+
+    const newStars = [];
+
+    for (let i = 0; i < numberOfStars; i++) {
+      newStars.push({
+        id: i,
+        size: Math.random() * 3 + 1,
+        x: Math.random() * 100,
+        y: Math.random() * 100,
+        opacity: Math.random() * 0.5 + 0.5,
+        animationDuration: Math.random() * 4 + 2,
+      });
+    }
+
+    setStars(newStars);
+  };
+  return (
+    <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+      {stars.map((star) => (
+        <div
+          key={star.id}
+          className="star animate-pulse-subtle"
+          style={{
+            width: star.size + "px",
+            height: star.size + "px",
+            top: star.y + "%",
+            left: star.x + "%",
+            opacity: star.opacity,
+            animationDuration: star.animationDuration + "s",
+          }}
+        />
+      ))}
+    </div>
+  );
+};
